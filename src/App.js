808b5a4eefb05bf870ea509cb705a4de1d778bd6
theCarlson <rjcarlson24@gmail.com>
2022-01-04 20:11:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
+import nftImg from './images/image-equilibrium.jpg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='card'>
+        <div className='card-content'>
+          <img className='card-content_img' src={nftImg} alt='' />
+        </div>
+        <div className='card-content_details'>
+          <p className='card-content_details-title'>Equilibrium #3429</p>
+          <p className='card-content_details-explanation'>
+            Our Equilibrium collection promotes balance and calm.
+          </p>
+        </div>
+        <div className='card-content_meta'>
+          <div className='card-content_meta-left'>
+            <svg
+              className='card-content_meta-left-pendant'
+              viewBox='0 3 15 15'
+              width='11'
+              height='18'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                d='M11 10.216 5.5 18 0 10.216l5.5 3.263 5.5-3.262ZM5.5 0l5.496 9.169L5.5 12.43 0 9.17 5.5 0Z'
+                fill='#00FFF8'
+              />
+            </svg>
+            <p>0.041ETH</p>
+          </div>
+          <div className='card-content_meta-right'>
+            <svg
+              className='card-content_meta-left-clock'
+              viewBox='0 2 20 15'
+              width='17'
+              height='17'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                d='M8.305 2.007a6.667 6.667 0 1 0 0 13.334 6.667 6.667 0 0 0 0-13.334Zm2.667 7.334H8.305a.667.667 0 0 1-.667-.667V6.007a.667.667 0 0 1 1.334 0v2h2a.667.667 0 0 1 0 1.334Z'
+                fill='#8BACD9'
+              />
+            </svg>
+            <p>3 days left</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
