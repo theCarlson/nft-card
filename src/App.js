af9f297e469b56fac941dba4ff805d71299051b1
theCarlson <rjcarlson24@gmail.com>
2022-01-21 18:13:43 -0500

    pull changes from github

--- src/App.js
+++ src/App.js
@@ -8,20 +8,25 @@ const App = () => {
     <div className='container'>
       <div className='card'>
         <div className='card-content'>
+          <div className='card-content_overlay'></div>
+          <svg
+            className='card-content_overlay-eye'
+            width='48'
+            height='48'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <g fill='none' fill-rule='evenodd'>
+              <path d='M0 0h48v48H0z' />
+              <path
+                d='M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15 10.01 0 18.54-6.22 22-15-3.46-8.78-11.99-15-22-15Zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10Zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6Z'
+                fill='#FFF'
+                fill-rule='nonzero'
+              />
+            </g>
+          </svg>
           <img className='card-content_img' src={nftImg} alt='' />
-          <div className='card-content_overlay'>
-            <svg width='48' height='48' xmlns='http://www.w3.org/2000/svg'>
-              <g fill='none' fill-rule='evenodd'>
-                <path d='M0 0h48v48H0z' />
-                <path
-                  d='M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15 10.01 0 18.54-6.22 22-15-3.46-8.78-11.99-15-22-15Zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10Zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6Z'
-                  fill='#FFF'
-                  fill-rule='nonzero'
-                />
-              </g>
-            </svg>
-          </div>
         </div>
+
         <div className='card-content_details'>
           <p className='card-content_details-title'>Equilibrium #3429</p>
 
@@ -29,6 +34,7 @@ const App = () => {
             Our Equilibrium collection promotes balance and calm.
           </p>
         </div>
+
         <div className='card-content_meta'>
           <div className='card-content_meta-left'>
             <svg
@@ -61,6 +67,7 @@ const App = () => {
             <p>3 days left</p>
           </div>
         </div>
+
         <div className='card-content_user'>
           <img className='card-content_user-avatar' src={avatar} alt='Avatar' />
           <div className='card-content_user-name'>
